21ace234b4e1a31d65b377cdbf960ca31f755ff6
LetsBash.de <90566696+LetsBashDE@users.noreply.github.com>
2021-09-12 18:53:36 +0200

    Update init.cmd

--- init.cmd
+++ init.cmd
@@ -3,4 +3,7 @@ rem This little thing must be in the same directory as the powershell script
 rem It will execute the Powershellscript for you with the pypass security settings
 
 powershell.exe -executionpolicy bypass -nologo -noprofile -file process.ps1
+echo.
+echo    ...Programm is ending...
+echo.
 timeout /t 5
