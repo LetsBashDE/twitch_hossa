c59ded12f69492b4c326169dc135bac0a8ed1afa
LetsBash.de <90566696+LetsBashDE@users.noreply.github.com>
2021-09-13 02:23:13 +0200

    Update init.cmd

--- init.cmd
+++ init.cmd
@@ -2,8 +2,18 @@
 rem This little thing must be in the same directory as the powershell script
 rem It will execute the Powershellscript for you with the pypass security settings
 
+echo Start elevating as an Administrator
+echo This is needed because hotkeys can only send global from an administrative application
+
+rem Unevelevated
+if "%1"=="psx" goto psx
+powershell.exe -executionpolicy bypass -nologo -noprofile -command "start-process init.cmd -verb runas -argument 'psx' -WorkingDirectory '%~dp0'"
+goto end
+
+rem Elevated
+:psx
+cd "%~dp0"
 powershell.exe -executionpolicy bypass -nologo -noprofile -file process.ps1
-echo.
-echo    ...Programm is ending...
-echo.
-timeout /t 5
+pause
+
+:end
