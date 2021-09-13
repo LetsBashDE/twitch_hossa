a7871539d64d7b9a7a902899c5be855d1a58509e
LetsBash.de <90566696+LetsBashDE@users.noreply.github.com>
2021-09-13 05:53:32 +0200

    Update init.cmd

--- init.cmd
+++ init.cmd
@@ -2,7 +2,7 @@
 rem This little thing must be in the same directory as the powershell script
 rem It will execute the Powershellscript for you with the pypass security settings
 
-rem goto execute
+goto execute
 
 if "%1"=="elevated" goto execute
 echo Start elevating as an Administrator
