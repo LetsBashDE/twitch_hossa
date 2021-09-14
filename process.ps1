e18d77d906e2badc1957aad51efabcabb10ac705
LetsBash.de <90566696+LetsBashDE@users.noreply.github.com>
2021-09-14 22:38:23 +0200

    Update process.ps1

--- process.ps1
+++ process.ps1
@@ -27,6 +27,171 @@ $channelid    = ""                           # Your channelid extracted by your 
 $latestfollow = ""                           # Your lastest follower on twitch
 $matchfollow  = ""                           # Compare value for change detection
 
+# Assemblies
+$codeHotkeySender = @"
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public static class HotkeySender {    
+    public enum InputType : uint {
+        INPUT_MOUSE = 0,
+        INPUT_KEYBOARD = 1,
+        INPUT_HARDWARE = 3
+    }
+
+    [Flags]
+    internal enum KEYEVENTF : uint
+    {
+        KEYDOWN = 0x0,
+        EXTENDEDKEY = 0x0001,
+        KEYUP = 0x0002,
+        SCANCODE = 0x0008,
+        UNICODE = 0x0004
+    }
+
+    [Flags]
+    internal enum MOUSEEVENTF : uint
+    {
+        ABSOLUTE = 0x8000,
+        HWHEEL = 0x01000,
+        MOVE = 0x0001,
+        MOVE_NOCOALESCE = 0x2000,
+        LEFTDOWN = 0x0002,
+        LEFTUP = 0x0004,
+        RIGHTDOWN = 0x0008,
+        RIGHTUP = 0x0010,
+        MIDDLEDOWN = 0x0020,
+        MIDDLEUP = 0x0040,
+        VIRTUALDESK = 0x4000,
+        WHEEL = 0x0800,
+        XDOWN = 0x0080,
+        XUP = 0x0100
+    }
+
+    // Master Input structure
+    [StructLayout(LayoutKind.Sequential)]
+    public struct lpInput {
+        internal InputType type;
+        internal InputUnion Data;
+        internal static int Size { get { return Marshal.SizeOf(typeof(lpInput)); } }            
+    }
+
+    // Union structure
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct InputUnion {
+        [FieldOffset(0)]
+        internal MOUSEINPUT mi;
+        [FieldOffset(0)]
+        internal KEYBDINPUT ki;
+        [FieldOffset(0)]
+        internal HARDWAREINPUT hi;
+    }
+
+    // Input Types
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MOUSEINPUT
+    {
+        internal int dx;
+        internal int dy;
+        internal int mouseData;
+        internal MOUSEEVENTF dwFlags;
+        internal uint time;
+        internal UIntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct KEYBDINPUT
+    {
+        internal short wVk;
+        internal short wScan;
+        internal KEYEVENTF dwFlags;
+        internal int time;
+        internal UIntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct HARDWAREINPUT
+    {
+        internal int uMsg;
+        internal short wParamL;
+        internal short wParamH;
+    }
+
+    private class unmanaged {
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern uint SendInput (
+            uint cInputs, 
+            [MarshalAs(UnmanagedType.LPArray)]
+            lpInput[] inputs,
+            int cbSize
+        );
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern short VkKeyScan(char ch);
+    }
+
+    internal static short VkKeyScan(char ch) {
+        return unmanaged.VkKeyScan(ch);
+    }
+
+    internal static uint SendInput(uint cInputs, lpInput[] inputs, int cbSize) {
+        return unmanaged.SendInput(cInputs, inputs, cbSize);
+    }
+
+    public static void SendScanCode(short scanCode) {
+        lpInput[] KeyInputs = new lpInput[1];
+        lpInput KeyInput = new lpInput();
+        // Generic Keyboard Event
+        KeyInput.type = InputType.INPUT_KEYBOARD;
+        KeyInput.Data.ki.wScan = 0;
+        KeyInput.Data.ki.time = 0;
+        KeyInput.Data.ki.dwExtraInfo = UIntPtr.Zero;
+
+
+        // Push the correct key
+        KeyInput.Data.ki.wVk = scanCode;
+        KeyInput.Data.ki.dwFlags = KEYEVENTF.KEYDOWN;
+        KeyInputs[0] = KeyInput;
+        SendInput(1, KeyInputs, lpInput.Size);
+        Thread.Sleep(200);
+        // Release the key
+        KeyInput.Data.ki.dwFlags = KEYEVENTF.KEYUP;
+        KeyInputs[0] = KeyInput;
+        SendInput(1, KeyInputs, lpInput.Size);
+
+        return;
+    }
+
+    public static void SendKeyboard(char ch) {
+        lpInput[] KeyInputs = new lpInput[1];
+        lpInput KeyInput = new lpInput();
+        // Generic Keyboard Event
+        KeyInput.type = InputType.INPUT_KEYBOARD;
+        KeyInput.Data.ki.wScan = 0;
+        KeyInput.Data.ki.time = 0;
+        KeyInput.Data.ki.dwExtraInfo = UIntPtr.Zero;
+
+
+        // Push the correct key
+        KeyInput.Data.ki.wVk = VkKeyScan(ch);
+        KeyInput.Data.ki.dwFlags = KEYEVENTF.KEYDOWN;
+        KeyInputs[0] = KeyInput;
+        SendInput(1, KeyInputs, lpInput.Size);
+
+        // Release the key
+        KeyInput.Data.ki.dwFlags = KEYEVENTF.KEYUP;
+        KeyInputs[0] = KeyInput;
+        SendInput(1, KeyInputs, lpInput.Size);
+
+        return;
+    }
+}
+"@
+Add-Type -TypeDefinition $codeHotkeySender
+
+
 
 function init_main {
     show_welcome
@@ -385,14 +550,25 @@ function init_detector {
 
 function keypress {
     
-    # Execute Keypress
+    # Write Keypress to console
     write-host ("Action: Hotkey F"+$Global:hotkey+" send") -ForegroundColor Cyan
-    if([Environment]::Is64BitOperatingSystem){
-         ./keypress64.exe $Global:hotkey
+    
+    # Execute Keypress
+    switch ($Global:hotkey) {
+        1 { [HotkeySender]::SendScanCode(0x70) }
+        2 { [HotkeySender]::SendScanCode(0x71) }
+        3 { [HotkeySender]::SendScanCode(0x72) }
+        4 { [HotkeySender]::SendScanCode(0x73) }
+        5 { [HotkeySender]::SendScanCode(0x74) }
+        6 { [HotkeySender]::SendScanCode(0x75) }
+        7 { [HotkeySender]::SendScanCode(0x76) }
+        8 { [HotkeySender]::SendScanCode(0x77) }
+        9 { [HotkeySender]::SendScanCode(0x78) }
+        10 { [HotkeySender]::SendScanCode(0x79) }
+        11 { [HotkeySender]::SendScanCode(0x7A) }
+        12 { [HotkeySender]::SendScanCode(0x7B) }
     }
-    else {
-         ./keypress32.exe $Global:hotkey
-    }
+
 }
 
 init_main
